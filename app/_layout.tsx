7c5d802b4b0f0abc827d29a0eb3b48171eb74700
Asif Alam <97513711+Asif-Alam1@users.noreply.github.com>
2025-05-12 16:16:55 +0300

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import "../global.css";
 
 import { Stack } from "expo-router";
@@ -5,10 +6,32 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "@/context/supabase-provider";
 import { useColorScheme } from "@/lib/useColorScheme";
 import { colors } from "@/constants/colors";
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
+
+// In your component:
 
 export default function AppLayout() {
 	const { colorScheme } = useColorScheme();
-
+	useEffect(() => {
+		const checkForUpdates = async () => {
+		  try {
+			const update = await Updates.checkForUpdateAsync();
+			
+			if (update.isAvailable) {
+			  console.log("Update available, downloading...");
+			  await Updates.fetchUpdateAsync();
+			  await Updates.reloadAsync();
+			} else {
+			  console.log("No updates available");
+			}
+		  } catch (error) {
+			console.error("Error checking for updates:", error);
+		  }
+		};
+	  
+		checkForUpdates();
+	  }, []);
 	return (
 		<AuthProvider>
 			<Stack screenOptions={{ headerShown: false, gestureEnabled: false }}>
